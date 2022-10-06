5bf7923cd91420916b01aa99660e48836d40be5f
Aaroney13 <aaronzonaman@gmail.com>
2022-10-06 15:11:37 -0400

    added description

--- Automate_Regression.py
+++ Automate_Regression.py
@@ -7,9 +7,14 @@
 
 
 # function that produces graphs
-# input category and time frame, save location output graph
-#def regres()
+# input category and start time and end time frame, save location -> output graph, list of headlines above the line
+# def regres():
 
 
-# function that outputs graphs
-# input array of categories and time frames
+
+# for loop
+# input array of categories and array time frames (pandas datetime objects) to def regres
+# startime [pd.datetime(12, 20, 2001) ]
+# endtime 
+# for x in range (0, len(startime))
+# regres(startime[0], endtime[0])
