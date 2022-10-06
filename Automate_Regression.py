9e8bbd4de8114e5b63558169d5d834857d612552
Aaroney13 <aaronzonaman@gmail.com>
2022-10-06 16:16:42 -0400

    added description for committing

--- Automate_Regression.py
+++ Automate_Regression.py
@@ -11,10 +11,11 @@
 # def regres():
 
 
-
 # for loop
 # input array of categories and array time frames (pandas datetime objects) to def regres
 # startime [pd.datetime(12, 20, 2001) ]
-# endtime 
-# for x in range (0, len(startime))
-# regres(startime[0], endtime[0])
+# endtime [pd.datetime(12, 20, 2001) ]
+# categories ['', '']
+# for x in range (0, len(startime)):
+#   for category in categories:
+#       regres(startime[0], endtime[0], categories)
