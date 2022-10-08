8759c1df4118c2b2c51387a2ed5fcc1dffc685dd
Aaroney13 <aaronzonaman@gmail.com>
2022-10-07 23:01:09 -0400

    added regression

--- Automate_Regression.py
+++ Automate_Regression.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.ticker import PercentFormatter
 import numpy as np
 import datetime as dt
 import os
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+
 
 # Goals
 # 1. Automate the regression (organic searches and page views)
@@ -15,17 +17,45 @@ os.chdir(r"C:\Users\aaron\OneDrive\Documents\quest\dbk_consulting")
 df = pd.read_excel(os.getcwd() + '/merged_titles.xlsx')
 df = df.drop(df.columns[0], axis=1)
 df['date_published'] = pd.to_datetime(df['date_published'])
-print(df)
+
+
 # function that produces graphs
 # input category and start time and end time frame, save location -> output graph (PNG), list of headlines (CSV) above the line
 def regres(start_time, end_time, category, save_location):
     df2 = df[(df['date_published'] >= start_time) & (df['date_published'] <= end_time)]
     df2 = df2[df2['category'].str.contains(category)]
-    print(len(df2))
+    date_string = str(start_time)[:10] + '_' + str(end_time)[:10]
+
+    # TESTING AREA
+    if (start_time <= pd.to_datetime('12/31/2019')):
+        #x_val = df2['date_published'].values.astype('datetime64[D]').reshape(-1, 1)
+        df2['date_published_reg'] = df2['date_published'].map(dt.datetime.toordinal)
+        X_val = df2['date_published_reg'].values.reshape(-1, 1)
+        Y_val = df2['Organic Searches'].values.reshape(-1, 1)
+        ridge = Ridge().fit(X_val, Y_val)
+        #print(ridge.predict(X_val))
+        reg = LinearRegression().fit(X_val, Y_val)
+        print(df)
+        print(reg.coef_)
+        #print(df2['date_published'].values.reshape(-1, 1))
+
+        print(len(df2))
+        #reg.pre
+        plt.scatter(df2['date_published'], df2['Organic Searches'], alpha=.4)
+        plt.title(date_string + ' ' + category)
+        plt.xlabel('date')
+        plt.ylabel('Organic Searches')
+       # plt.plot(df2['date_published'], reg.predict(X_val), color="blue", linewidth=1)
+        plt.plot(df2['date_published'], ridge.predict(X_val), color="blue", linewidth=1)
+        plt.show()
+        plt.clf()
 
 
-date_range = pd.date_range(start='12/31/2019', end='8/1/2022', freq='Q')
-print(date_range)
+date_range = pd.date_range(start='12/31/2019', end='8/1/2022', freq='Y')
+
+# TEST DATE
+# date_range = [pd.to_datetime(date_range[0]),  pd.to_datetime(date_range[-1])]
+
 # for loop
 # input array of categories and array time frames (pandas datetime objects) to def regres
 categories = ['news']
