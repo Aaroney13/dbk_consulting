059ad86ba6767eac2492af38b240e59f0c636177
Aaroney13 <aaronzonaman@gmail.com>
2022-10-16 11:25:49 -0400

    created testing keywords

--- testing_keywords.py
+++ testing_keywords.py
@@ -1,20 +1,23 @@
 import pandas as pd
 import scipy.stats as stats
 import os 
+import matplotlib.pyplot as plt
 
 # Goal: test keywords to see if mean is higher than complement in group
 
-# Check assumptions of t test
-# -unequal variances
-
-# perform t test
 os.chdir(r"C:\Users\aaron\OneDrive\Documents\quest\dbk_consulting")
 df = pd.read_excel(os.getcwd() + '/merged_titles.xlsx')
 df = df.drop(df.columns[0], axis=1)
 df['Page Title'] = df['Page Title'].str.lower()
-df = df[df['category'].str.contains('campus')]
+#df = df[df['category'].str.contains('campus')]
 #test = df['Organic Searches'][df['Page Title'].str.contains('umd')]
-word = 'art'
-print(stats.ttest_ind( df['Organic Searches'][~df['Page Title'].str.contains(word)], df['Organic Searches'][df['Page Title'].str.contains(word)], equal_var=False))
+word = 'review'
+
+# doesnt contain word
+print(df['Organic Searches'][~df['Page Title'].str.contains(word)].describe())
+
+# contains word
+print(df['Organic Searches'][df['Page Title'].str.contains(word)].describe())
+print(df['Organic Searches'][df['Page Title'].str.contains(word)])
 
 #print(stats.levene(df['Organic Searches'][~df['Page Title'].str.contains(word)], df['Organic Searches'][df['Page Title'].str.contains(word)], center= 'mean'))
