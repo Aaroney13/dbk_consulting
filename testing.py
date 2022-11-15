f5ad471538b5e1f06662b15c28ac2195c89edbe2
Aaroney13 <aaronzonaman@gmail.com>
2022-11-15 12:58:59 -0500

    updated model

--- testing.py
+++ testing.py
@@ -13,10 +13,11 @@ df['question_mark']=0
 df['hit'] = df['log_organic'] > 1
 
 
-#df['diversions'] = df['category'].apply(lambda x: 1 if x.contains('diversions') else 0)
 df['diversions'] = df['category'].str.contains('diversions')
 df['question_mark'] = df['Page Title'].str.contains('\?')
+
 df['date_published'] = pd.to_datetime(df['date_published'])
+#df = df[df['date_published'].dt.year >= 2020]
 
 df['spring_semester'] = ((df['date_published'].dt.month > 1) & (df['date_published'].dt.month < 6))
 df['fall_semester'] = ((df['date_published'].dt.month > 8) & (df['date_published'].dt.month < 13))
@@ -28,9 +29,13 @@ complexity_list = []
 def complexity(df_):
     #complexity_list.append(textstat.flesch_kincaid_grade(df_))
     complexity_list.append(textstat.flesch_reading_ease(df_))
+    return df_
    #complexity_list.append(textstat.smog_index(df_))
 df['Page Title'].apply(lambda x: complexity(x))
 df['complexity'] = complexity_list
+
+df['complexity_hard'] = (df['complexity'] <= 60).astype(bool)
+df['complexity_easy'] = (df['complexity'] >= 70).astype(bool)
 
 df['complexity_standard'] = ((df['complexity'] >= 60) & (df['complexity'] < 70)).astype(bool)
 df['complexity_easy'] = ((df['complexity'] >= 70) & (df['complexity'] < 80)).astype(bool)
@@ -40,15 +45,17 @@ df['complex_spring_easy'] = df['complexity_easy'] & df['spring_semester']
 df['complex_fall_easy'] = df['complexity_easy'] & df['fall_semester']
 df['complex_off_standard'] = df['complexity_standard'] & df['off_season']
 df['complex_off_easy'] = df['complexity_easy'] & df['off_season']
-
-
-
+df['contain_review'] = df['Page Title'].str.contains('Review', case=False)
+df['contain_sga'] = df['Page Title'].str.contains('SGA', case=False)
+df['contain_umd'] = df['Page Title'].str.contains('UMD', case=False)
+df['contain_num'] = df['Page Title'].str.contains('\d+')
+print(df['contain_umd'])
 #rint(complexity_list)
 
 YVar = df[['hit']]
 # df['aa'] = np.asarray(df['complexity'])
 # df['aa'].to_csv(r"C:\Users\aaron\OneDrive\Documents\quest\aa.csv")
-XVar = df[['spring_semester', 'fall_semester', 'complex_off_standard', 'complex_off_easy', 'complex_spring_standard', 'complex_fall_standard', 'complex_spring_easy', 'complex_fall_easy', 'question_mark']]
+XVar = df[['spring_semester', 'fall_semester', 'complex_off_standard', 'question_mark', 'contain_review',  'complexity_hard', 'contain_umd', 'complex_fall_easy', 'complex_spring_easy']]
 
 X_train, X_test, y_train, y_test = train_test_split(XVar, YVar, test_size=0.2)
 
@@ -57,30 +64,27 @@ X_train, X_test, y_train, y_test = train_test_split(XVar, YVar, test_size=0.2)
 print(y_train)
 #Model = sm.Logit(YVar, XVar).fit()
 Model = sm.Logit(y_train, X_train).fit()
+
 predictions = Model.predict(X_test)
 
-predictions_to_int = predictions.apply(lambda x: 1 if x > .5 else 0)
+predictions_to_int = predictions.apply(lambda x: 1 if x > .6 else 0)
 
 
-# print(type(y_test))
-# print(type(predictions_to_int))
-# print(predictions_to_int)
-# print(y_test)
-print(type(y_test.astype(int)['hit'] - predictions_to_int))
-print(y_test.astype(int)['hit'] - predictions_to_int)
-print(((y_test.astype(int)['hit'] - predictions_to_int) == 0).value_counts().iloc[0])
+((y_test.astype(int)['hit'] - predictions_to_int) == 0)
 correct = ((y_test.astype(int)['hit'] - predictions_to_int) == 0).value_counts().iloc[0]
 incorrect = ((y_test.astype(int)['hit'] - predictions_to_int) == 0).value_counts().iloc[1]
-print('predicted ' + str(correct) + ' correctly and ' + str(incorrect) + ' incorrectly ' + 'this represents a ' + str(correct/len(y_test)) + ' success rate')
 
+print(predictions)
+
+#plt.scatter(predictions.index, predictions)
+#plt.show()
 #print((y_test.astype(int) - predictions.astype(int)))
 #print(predictions)
 ##print(y_test)
 #((y_test - predictions) != 0).to_csv(r"C:\Users\aaron\OneDrive\Documents\quest\predicted_wrong.csv")
 
 print(Model.summary())
-
-
+print('predicted ' + str(correct) + ' correctly and ' + str(incorrect) + ' incorrectly ' + 'this represents a ' + str(correct/len(y_test)) + ' success rate')
 # df['log_Pageviews'] = np.log(df['Pageviews'] + 1)
 # df['hit'] = df['log_Pageviews'] > 2.17
 
@@ -90,7 +94,8 @@ print(Model.summary())
 # print(Model2.summary())
 # df['complexity'] = np.log(df['complexity'] + .1)
 # ax = df['complexity'].plot.kde()
-
-
+# get indices for incorrect guesses
+print(((y_test.astype(int)['hit'] - predictions_to_int) != 0).where(((y_test.astype(int)['hit'] - predictions_to_int) != 0)).dropna().index)
+df.loc[((y_test.astype(int)['hit'] - predictions_to_int) != 0).where(((y_test.astype(int)['hit'] - predictions_to_int) != 0)).dropna().index].to_csv(r"C:\Users\aaron\OneDrive\Documents\quest\wrong_guesses.csv")
 
 #plt.show()
